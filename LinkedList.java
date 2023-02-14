b67002f14ccf77cba86e6b3f88df5ea0d449d521
Lozing <lozingyt@gmail.com>
2023-02-14 21:45:44 +0000

    All done for linked list?

--- LinkedList.java
+++ LinkedList.java
@@ -1,16 +1,19 @@
 public class LinkedList<T>{
-    Node<T> head;
+    private Node<T> head;
+    int count = 0;
 
     public LinkedList(){
         this.head = null;
     }
 
     public boolean isEmpty(){
-        return head == null ? true:false;
+        return count == 0 ? true:false;
     }
 
     public void insertBeginning(T value){
         Node<T> n = new Node<T>(value);
+        count++;
+
         // Case 1: The head is null
         if(head == null){
             head = n;
@@ -24,6 +27,8 @@ public class LinkedList<T>{
 
     public void insertEnd(T value){
         Node<T> n = new Node<T>(value);
+        count++;
+
         // Case 1: The head is null
         if(head == null){
             head = n;
@@ -31,30 +36,128 @@ public class LinkedList<T>{
 
         // Case 2: The head is not null
         Node<T> end = head;
-        while(end != null || end.getNext() != null){
+        while(end.getNext() != null){
             end = end.getNext();
         }
 
         end.setNext(n);
     }
 
-    public void insertAt(int index, T value){
+    public void insertAt(int index, T value) throws ArrayIndexOutOfBoundsException {
+        // Check that the index to insert into is valid
+        if(index > count){
+            throw new ArrayIndexOutOfBoundsException();
+        }
 
+        // Increment the count and create the new node
+        count++;
+        Node<T> n = new Node<T>(value);
+
+        // Case 1: head is null
+        if(head == null){
+            head = n;
+            return;
+        }
+        // Case 2: head is not null but index is 0
+        else if(index == 0){
+            n.setNext(head);
+            head = n;
+            return;
+        }
+
+        // Case 3: head is not null and index is not 0
+        Node<T> pos = head;
+        for(int i = 0; i < index - 1; i++){
+            pos = pos.getNext();
+        }
+
+        Node<T> ahead = pos.getNext();
+        pos.setNext(n);
+        n.setNext(ahead);
     }
 
     public void delete(T value){
+        // Case 1: There are no elements
+        if(count == 0){
+            return;
+        }
 
+        Node<T> end = head;
+
+        // Case 2: The head is the only element and is the value
+        if(head.value == value && count == 1){
+            head = null;
+            return;
+        }
+        // Case 3: The head is not the only element and is the value
+        else if(head.value == value){
+            head = head.getNext();
+        }
+
+        // Case 4: The head is not the only element and is not the value
+        while(end.getNext() != null){
+            if(end.getNext().value == value){
+                end.setNext(end.getNext().getNext());
+                count--;
+                break;
+            }
+            end = end.getNext();
+        }
     }
 
     public void deleteAll(T value){
-
+        for(int i = 0; i < count; i++){
+            this.delete(value);
+        }
     }
 
-    public void deleteAt(int index, T value){
+    public void deleteAt(int index){
+        // Check that the index to delete is valid
+        if(index >= count){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        // Case 1: The length is 1 and the head is being deleted
+        if(count == 1 && index == 0){
+            head = null;
+            return;
+        }
+        // Case 2: The length is not 1 and the head is being deleted
+        else if(index == 0){
+            head = head.getNext();
+            return;
+        }
+        
+        Node<T> pos = head;
+        for(int i = 0; i < index - 1; i++){
+            pos = pos.getNext();
+        }
+        // Case 3: The length is not 1 and the last element is being deleted
+        if(index + 1 == count){
+            pos.setNext(null);
+        }
+        // Case 4: The length is not 1 and the last element is not the one being deleted
+        else{
+            pos.setNext(pos.getNext().getNext());
+        }
 
     }
 
     public void reset(){
+        head = null;
+    }
 
+    public String toString(){
+        String output = "[";
+        Node<T> end = head;
+        while(end != null){
+            if(head != end){
+                output += ", ";
+            }
+            output += end.value.toString();
+            end = end.getNext();
+        }
+        output += "]";
+        return output;
     }
 }
---
+++ TestApp.java
@@ -0,0 +1,24 @@
+public class TestApp {
+    public static void main(String[] args){
+        LinkedList<Integer> l = new LinkedList<>();
+        l.insertBeginning(2);
+        System.out.println(l);
+        l.insertEnd(3);
+        System.out.println(l);
+        l.insertAt(1, 1);
+        System.out.println(l);
+        l.insertAt(0, 0);
+        System.out.println(l);
+        l.insertAt(4, 4);
+        System.out.println(l);
+        l.insertAt(5, 10);
+        l.insertAt(5, 10);
+        System.out.println(l);
+        l.delete(10);
+        System.out.println(l + " No 10");
+        l.insertAt(5, 10);
+        l.insertAt(5, 10);
+        l.deleteAll(10);
+        System.out.println(l);
+    }
+}
