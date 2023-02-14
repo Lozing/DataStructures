28c2f427f852631cb08a0a9665ae7edd6c0e30a2
Lozing <lozingyt@gmail.com>
2023-02-14 17:19:16 +0000

    outlined

--- LinkedList.java
+++ LinkedList.java
@@ -1,3 +1,39 @@
-public class LinkedList{
-    
+public class LinkedList<T>{
+    Node<T> head;
+
+    public LinkedList(){
+        this.head = null;
+    }
+
+    public boolean isEmpty(){
+        return head == null ? true:false;
+    }
+
+    public void insertEnd(T value){
+
+    }
+
+    public void insertBeginning(T value){
+
+    }
+
+    public void insertAt(int index, T value){
+
+    }
+
+    public void delete(T value){
+
+    }
+
+    public void deleteAll(T value){
+
+    }
+
+    public void deleteAt(int index, T value){
+
+    }
+
+    public void reset(){
+
+    }
 }
--- Node.java
+++ Node.java
@@ -1,3 +1,13 @@
-public class Node {
-    
+public class Node<T> {
+    T value;
+    Node<T> next;
+
+    public Node(T value){
+        this.value = value;
+        this.next = null;
+    }
+
+    public void setNext(Node<T> next){
+        this.next = next;
+    }
 }
