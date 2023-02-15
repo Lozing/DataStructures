31e8017f1580c37b8ee70326a525b17a7aa54116
Lozing <lozingyt@gmail.com>
2023-02-15 12:51:19 +0000

    editied for testing

--- TestApp.java
+++ TestApp.java
@@ -1,33 +1,16 @@
-import LinkedListDataStructure.LinkedList;
+import StackDataStructure.*;;
 
 public class TestApp {
     public static void main(String[] args){
-        LinkedList<Integer> l = new LinkedList<>();
-        l.insertBeginning(2);
-        System.out.println(l);
-        l.insertEnd(3);
-        System.out.println(l);
-        l.insertAt(1, 1);
-        System.out.println(l);
-        l.insertAt(0, 0);
-        System.out.println(l);
-        l.insertAt(4, 4);
-        System.out.println(l);
-        l.insertAt(5, 10);
-        l.insertAt(5, 10);
-        System.out.println(l);
-        l.delete(10);
-        System.out.println(l + " No 10");
-        l.insertAt(5, 10);
-        l.insertAt(5, 10);
-        l.deleteAll(10);
-        System.out.println(l);
-
-        for(Integer i: l){
-            System.out.println(i);
-        }
-        l.reverse();
-        System.out.println(l);
-        System.out.println(l.getHead().value);
+        Stack<Integer> s = new Stack<>(5, new Integer[] {2, 4});
+        System.out.println(s);
+        s.push(3);
+        System.out.println(s);
+        s.push(3);
+        System.out.println(s);
+        s.push(3);
+        System.out.println(s);
+        System.out.println(s.pop());
+        System.out.println(s);
     }
 }
